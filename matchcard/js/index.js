e37a24119b36b05fda3a34ca1b95d3fc3304fabd
unknown <396016240@qq.com>
2019-06-19 11:07:19 +0800

    a

--- matchcard/js/index.js
+++ matchcard/js/index.js
@@ -2,6 +2,7 @@
     function initPage(){
         cloneCard();
         startTimer();
+        bindListener();
     }
 
     //初始化卡牌
@@ -10,9 +11,31 @@
         let cardDom,tableDom,tempDom;
         tableDom=document.querySelector(".table");
         cardDom=document.querySelector(".card");
-        for(let i=0;i<cardAmount;i++){
-            
+        for(let i=1;i<cardAmount;i++){
+            tempDom=cardDom.cloneNode(true);
+            tableDom.appendChild(tempDom);
         }
+    }
+
+    //设置事件监听
+    function bindListener(){
+        let tableDom=document.querySelector(".table");
+        tableDom.addEventListener("click",(e)=>{
+            let target=e.target;
+            if(target.className==="table"){
+                return ;
+            }else{
+                while(target.className.indexOf("card")<0){
+                    target=target.parentNode;
+                }
+                let className=target.className;
+                if(className.indexOf("rotate")>=0){
+                    target.className=className.replace("rotate","");
+                }else{
+                    target.className=className+" rotate";
+                }
+            }
+        },false);
     }
 
     //设置定时器
