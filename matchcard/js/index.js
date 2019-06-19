36638eb5dcf8f029724279f861196bf543d5e195
unknown <396016240@qq.com>
2019-06-19 13:25:35 +0800

    a

--- matchcard/js/index.js
+++ matchcard/js/index.js
@@ -1,5 +1,11 @@
 (function(){
+    let randomCards;
+    //卡牌总数
+    let cardAmount=24;
+    let timer;
+
     function initPage(){
+        randomCard();
         cloneCard();
         startTimer();
         bindListener();
@@ -7,24 +13,53 @@
 
     //初始化卡牌
     function cloneCard(){
-        let cardAmount=24;
+        let cardWidth=80;
+        let cardHeight=-120;
         let cardDom,tableDom,tempDom;
         tableDom=document.querySelector(".table");
         cardDom=document.querySelector(".card");
-        for(let i=1;i<cardAmount;i++){
+        tableDom.removeChild(cardDom);
+        for(let i=0;i<cardAmount;i++){
             tempDom=cardDom.cloneNode(true);
+            let front=tempDom.querySelector(".front");
+            //根据随机卡牌设置图片偏移量
+            front.style.backgroundPosition=(randomCards[i][0]*cardWidth)+"px "+(randomCards[i][1]*cardHeight)+"px";
             tableDom.appendChild(tempDom);
         }
+    }
+
+    //随机卡牌
+    function randomCard(){
+        let maxX=12;
+        let maxY=4;
+        let originCards=[];
+        randomCards=[];
+        //先产生对牌
+        for(let i=0;i<(cardAmount/2);i++){
+            let randomX=Math.floor(Math.random()*maxX);
+            let randomY=Math.floor(Math.random()*maxY);
+            originCards.push([randomX,randomY]);
+        }
+        originCards=originCards.concat(originCards);
+        //再生成随机卡牌
+        for(let i=0;i<cardAmount;i++){
+            let random=Math.floor(Math.random()*originCards.length);
+            randomCards.push(originCards[random]);
+            originCards.splice(random,1);
+        }
+        console.log(randomCards);
     }
 
     //设置事件监听
     function bindListener(){
         let tableDom=document.querySelector(".table");
+        let lastDom,removeCard=0;
         tableDom.addEventListener("click",(e)=>{
             let target=e.target;
             if(target.className==="table"){
                 return ;
             }else{
+                //多节点事件，在父节点上单次绑定
                 while(target.className.indexOf("card")<0){
                     target=target.parentNode;
                 }
@@ -34,24 +69,48 @@
                 }else{
                     target.className=className+" rotate";
                 }
+                //判断是否相同卡牌
+                if(lastDom===undefined){
+                    lastDom=target;
+                }else{
+                    let lastFront=lastDom.querySelector(".front");
+                    let front=target.querySelector(".front");
+                    if(lastFront.style.backgroundPosition==front.style.backgroundPosition){
+                        setTimeout(()=>{
+                            lastDom.className=lastDom.className+" removed";
+                            target.className=target.className+" removed";
+                            removeCard+=2;
+                            lastDom=undefined;
+                            if(removeCard===cardAmount){
+                                setTimeout(winGame,600);
+                            }
+                        },1200);
+                    }else{
+                        setTimeout(()=>{
+                            lastDom.className=lastDom.className.replace("rotate","");
+                            target.className=target.className.replace("rotate","");
+                            lastDom=undefined;
+                        },1200);
+                    }
+                }
             }
         },false);
     }
 
     //设置定时器
-    let timer;
     function startTimer(){
         let timeDom=document.getElementById("time");
-        let second=200;
+        let second=100;
         timeDom.innerHTML=second;
         timer=setTimeout(function loop(){
             timeDom.innerHTML=--second;
             if(second===0){
                 setTimeout(()=>{
                     alert("游戏失败");
+                    initPage();
                 },0);
             }else{
-                setTimeout(loop,1000);
+                timer=setTimeout(loop,1000);
             }
         },1000);
     }
